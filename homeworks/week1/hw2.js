e0a4a2029d6a0383bac503e31d581fcee4e74b49
Eife <husky76101@gmail.com>
2018-09-08 15:39:47 +0800

    'wk1fixed'

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,3 +1,12 @@
-function capitalize(str) {
-  
+function capitalize (str) {
+  var result = '';
+  if (str[0]>='a' && str[0]<='z') {
+    result += str[0].toUpperCase() + str.substr(1);
+  } else {
+    result += str;
+  }
+    return result;
 }
+
+console.log(capitalize(',hello')); 
+
--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,19 @@
 function join(str, concatStr) {
-
+  var result = '';
+  for (var i=0; i<str.length; i++) {
+    result += str[i] + concatStr;
+  }
+  return result.substr(0, result.length-1);
 }
 
-function repeat(str, times) {
+console.log(join(["a", "b", "c"], '!'));
 
+function repeat (str, times) {
+ var result = '';
+ for (var i=1; i<=times; i++) {
+   result += str;
+  }
+ return result;	
 }
+
+console.log(repeat('yoyo', 2));
